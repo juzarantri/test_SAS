d340626f8137c25c2f69b5f991540597da831647
iamjuzarantri <juzarantri786@gmail.com>
2021-05-22 22:37:08 +0530

    Login module done with bootstrap sign up left

---
+++ SAS_Student_Attendance_System/SAS_Student_Attendance_System/asgi.py
@@ -0,0 +1,16 @@
+"""
+ASGI config for SAS_Student_Attendance_System project.
+
+It exposes the ASGI callable as a module-level variable named ``application``.
+
+For more information on this file, see
+https://docs.djangoproject.com/en/3.2/howto/deployment/asgi/
+"""
+
+import os
+
+from django.core.asgi import get_asgi_application
+
+os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'SAS_Student_Attendance_System.settings')
+
+application = get_asgi_application()
