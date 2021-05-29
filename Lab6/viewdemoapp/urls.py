267a5f331cebd410fb0e897b53f231c10e0aebc7
Juzar Antri <82743796+juzarantri@users.noreply.github.com>
2021-05-29 12:31:24 +0530

    Delete Lab6 directory

--- Lab6/viewdemoapp/urls.py
+++
@@ -1,7 +0,0 @@
-from viewdemoapp import views
-from django.conf.urls import url
-urlpatterns = [
-    url(r'^index/$',views.HomePage.as_view()),
-    url(r'^aboutme/$',views.aboutMe),
-    url(r'^contactme/$',views.contactMe),
-]
